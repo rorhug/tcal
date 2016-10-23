e0c0427709268648fe269f39b8bbf0fc2431c101
Rory Hughes <rory@roryhughes.net>
2016-10-23 12:06:36 +0100

    comment que job method

--- app/models/que_job.rb
+++ app/models/que_job.rb
@@ -1,22 +1,22 @@
 class QueJob < ActiveRecord::Base
-  def self.for_users(users, job_class: nil)
-    q = QueJob.all
+  # def self.for_users(users, job_class: nil)
+  #   q = QueJob.all
 
-    elsif users.count > 1
-      q = q.where("args->>0 IN (?)", users.pluck(:id))
-    elsif users.count == 1
-      q = q.where("args->>0 = ?", users.first.id)
-    end
+  #   elsif users.count > 1
+  #     q = q.where("args->>0 IN (?)", users.pluck(:id))
+  #   elsif users.count == 1
+  #     q = q.where("args->>0 = ?", users.first.id)
+  #   end
 
-    if job_class
-      q = .where(job_class: "SyncTimetable")
-  end
+  #   if job_class
+  #     q = .where(job_class: "SyncTimetable")
+  # end
 
-  def self.for_user(user, job_class: nil)
-    if users.is_a?(User)
-      q = q.where("args->>0 = ?", users.id)
-    elsif users.is_a?(Fixnum)
-      q = q.where("args->>0 = ?", users)
-    User::ActiveRecord_Relation
-  end
+  # def self.for_user(user, job_class: nil)
+  #   if users.is_a?(User)
+  #     q = q.where("args->>0 = ?", users.id)
+  #   elsif users.is_a?(Fixnum)
+  #     q = q.where("args->>0 = ?", users)
+  #   User::ActiveRecord_Relation
+  # end
 end
