0348a39f4c92ef578030732f6393b4a02d2fe19d
Rory Hughes <rory@roryhughes.net>
2016-10-25 01:21:38 +0100

    force click trackin

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "Tcal <tcal@tcal.me>"
+  default from: "Tcal <tcal@tcal.me>", "X-MSYS-API" => {options: {open_tracking: true, click_tracking: true}}.to_json
   layout 'mailer'
 end
