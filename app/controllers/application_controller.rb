772152b88611baaa9b328e1e60e9818f8890040a
Rory Hughes <rory@roryhughes.net>
2016-10-25 22:24:34 +0100

    fix login redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < ActionController::Base
 
   private
     def authenticate!
-      current_user.set_joined_at_if_invited!
+      current_user.set_joined_at_if_invited! if current_user
       unless session[:user_id] && current_user
         reset_session
         redirect_to root_path
