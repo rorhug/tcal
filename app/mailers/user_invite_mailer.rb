cf8ac37539c4d1eed48dbaf24658c41cab510938
Rory Hughes <rory@roryhughes.net>
2016-10-25 16:20:27 +0100

    blank inviter in email if inviter was an admin

--- app/mailers/user_invite_mailer.rb
+++ app/mailers/user_invite_mailer.rb
@@ -3,6 +3,7 @@ class UserInviteMailer < ApplicationMailer
   def notify(user)
     @user = user
     @inviter = user.invited_by
+    @inviter = nil if @inviter && @inviter.is_admin
     @subject = "Invite to Tcal#{ " from #{@inviter.my_tcd_username_estimate}" if @inviter }"
     mail(
       to: @user.email,
