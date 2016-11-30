bbc0b17de2fe8585a91d0afab9d600f7530d3271
Rory Hughes <rory@roryhughes.net>
2016-11-30 13:46:11 +0000

    Kill silly sentry logs

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,8 +1,9 @@
-Raven.configure do |config|
-  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+Rails.application.config.before_configuration do
+  Raven.configuration.silence_ready = true
 end
 
 Raven.configure do |config|
+  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   config.dsn = Rails.application.secrets.sentry_dns_app_prod if Rails.env.production?
   config.environments = ['production']
 end
