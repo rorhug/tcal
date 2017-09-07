9e4c03c87d21370ee0ccc7131e4feb8303911cb3
Rory Hughes <rory@roryhughes.net>
2017-09-08 00:11:35 +0100

    update conf

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,6 +4,6 @@ end
 
 Raven.configure do |config|
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-  config.dsn = Rails.application.secrets.sentry_dns_app_prod if Rails.env.production?
+  config.dsn = Rails.application.secrets.sentry_dsn_app_prod if Rails.env.production?
   config.environments = ['production']
 end
