35812278a22c909f10363200a4d94cb82fafc8c7
Rory Hughes <rory@roryhughes.net>
2017-09-23 22:37:34 +0100

    add que service file

--- config/initializers/que.rb
+++ config/initializers/que.rb
@@ -1,12 +1,12 @@
-QUE_EXCLUE_LOG_EVENTS = [:job_unavailable]
+QUE_EXCLUDE_LOG_EVENTS = [:job_unavailable]
 
 if $IS_QUE
-  # Que.wake_interval = 2.seconds
+  Que.wake_interval = 1.seconds
   Que.mode = :off
-  Que.worker_count = (ENV['QUE_WORKERS'] || 4)
+  Que.worker_count = 4
   # Que.logger = Logger.new("#{Rails.root}/log/worker_#{Rails.env}.log")
   Que.log_formatter = proc do |data|
-    if QUE_EXCLUE_LOG_EVENTS.exclude?(data[:event])
+    if QUE_EXCLUDE_LOG_EVENTS.exclude?(data[:event])
       JSON.dump(data)
     end
   end
